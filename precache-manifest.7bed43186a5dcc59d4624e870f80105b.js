3b476d9d5ad960baec595eca0292feced16ad6dd
mdrae <mdrae0@gmail.com>
2018-12-14 21:03:31 -0600

    Updates

---
+++ precache-manifest.7bed43186a5dcc59d4624e870f80105b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb4cb2e5cd04bd4e3779",
+    "url": "/pong-app/static/css/main.9e102501.chunk.css"
+  },
+  {
+    "revision": "eb4cb2e5cd04bd4e3779",
+    "url": "/pong-app/static/js/main.eb4cb2e5.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/pong-app/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "a434f3067d2e9d53802c",
+    "url": "/pong-app/static/js/runtime~main.a434f306.js"
+  },
+  {
+    "revision": "52a6484f2699a665f5dbaf9d0531b684",
+    "url": "/pong-app/static/media/digital_counter_7.52a6484f.ttf"
+  },
+  {
+    "revision": "e8e968d374b355f67cb5f20ee7abe458",
+    "url": "/pong-app/index.html"
+  }
+];
